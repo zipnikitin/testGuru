087a7c8916341d3de25710cedeb5fc21fe07972e
zipnikitin <40566232+zipnikitin@users.noreply.github.com>
2021-06-10 20:28:00 +0300

    Update 20210607140214_create_users.rb

--- testGuru/db/migrate/20210607140214_create_users.rb
+++ testGuru/db/migrate/20210607140214_create_users.rb
@@ -1,7 +1,6 @@
 class CreateUsers < ActiveRecord::Migration[6.1]
   def change
     create_table :users do |t|
-      t.integer :author_id
       t.string :name
       t.string :email
       t.string :password
