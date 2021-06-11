2649a8ac7ed81c8e39557479f67ceb4d289ce51e
zipnikitin <40566232+zipnikitin@users.noreply.github.com>
2021-06-11 17:28:05 +0300

    Update 20210607130611_create_tests.rb

--- testGuru/db/migrate/20210607130611_create_tests.rb
+++ testGuru/db/migrate/20210607130611_create_tests.rb
@@ -1,7 +1,7 @@
 class CreateTests < ActiveRecord::Migration[6.1]
   def change
     create_table :tests do |t|
-      t.references :author, null: false, foreign_key: 'user_id'
+      t.references :author, null: false, foreign_key: { to_table: :user }
       t.references :category, null: false, foreign_key: true
       t.string :tittle, null: false
       t.integer :level, default: 1, null: false
