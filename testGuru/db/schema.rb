89a63b18de69c73fd01eb6d36922eece66df4a26
zipnikitin <40566232+zipnikitin@users.noreply.github.com>
2021-06-11 17:29:45 +0300

    Update schema.rb

--- testGuru/db/schema.rb
+++ testGuru/db/schema.rb
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20210610155313) do
   end
 
   create_table "tests", force: :cascade do |t|
+    t.integer  "author_id",               null: false
     t.integer  "category_id"
     t.datetime "created_at"
     t.datetime "updated_at"
@@ -30,6 +31,7 @@ ActiveRecord::Schema.define(version: 20210610155313) do
   end
 
   create_table "users", force: :cascade do |t|
+    
     t.string   "name"
     t.string   "email"
     t.string   "password"
