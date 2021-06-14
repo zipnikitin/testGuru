fabc32e9519d5a5e96a85e6c5cf78391decc8910
zipnikitin <zipnikitin@gmail.com>
2021-06-14 19:50:48 +0300

    First

--- testGuru/app/models/user.rb
+++ testGuru/app/models/user.rb
@@ -1,3 +1,9 @@
-class User < ApplicationRecord
+class User < ActiveRecord::Base
+  attr_accessor :tests
 
+  def tests_used_to_solve(current_level)
+    @tests.each do |test|
+      puts ('Тесты ' + test.level.to_s + ' уровня: ' + test.title.to_s) if test.level == current_level
+    end
+  end
 end
