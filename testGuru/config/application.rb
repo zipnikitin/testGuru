b0f52e906088fcb0e5d3078ba4abbbf22ba5dc5d
zipnikitin <zipnikitin@gmail.com>
2021-06-17 00:06:15 +0300

    FNC

--- testGuru/config/application.rb
+++ testGuru/config/application.rb
@@ -1,12 +1,23 @@
-require File.expand_path('../boot', __FILE__)
+require_relative "boot"
 
-require 'rails/all'
+require "rails/all"
 
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module TestGuru
   class Application < Rails::Application
     config.timezone = 'Moscow'
     config.i18n.default_locale = :ru
+    config.load_defaults 6.1
+
+    # Configuration for the application, engines, and railties goes here.
+    #
+    # These settings can be overridden in specific environments using the files
+    # in config/environments, which are processed later.
+    #
+    # config.time_zone = "Central Time (US & Canada)"
+    # config.eager_load_paths << Rails.root.join("extras")
   end
 end
