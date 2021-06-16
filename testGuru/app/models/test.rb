2665ed3265f6db7c75611b9952c1d51b23473939
zipnikitin <zipnikitin@gmail.com>
2021-06-17 00:07:35 +0300

    Third

--- testGuru/app/models/test.rb
+++ testGuru/app/models/test.rb
@@ -1,3 +1,5 @@
-class Test < ApplicationRecord
-  
+class Test < ActiveRecord::Base
+  def self.tests_named(category_name)
+    Test.joins('JOIN categories ON tests.category_id = categories.id').where(level: category_name ).order(type: :desc)
+  end
 end
--- testGuru/app/models/user.rb
+++ testGuru/app/models/user.rb
@@ -1,3 +1,5 @@
-class User < ApplicationRecord
-
+class User < ActiveRecord::Base
+  def tests_with_level(current_level)
+    User.joins('JOIN tests ON user.id = tests.id').where(level: current_level )
+  end
 end
