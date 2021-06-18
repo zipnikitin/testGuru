80474b2ee40c05b0fa8dfbd6eb942ad2be6df371
zipnikitin <40566232+zipnikitin@users.noreply.github.com>
2021-06-18 23:47:02 +0300

    Update test.rb

--- testGuru/app/models/test.rb
+++ testGuru/app/models/test.rb
@@ -1,7 +1,7 @@
 
 class Test < ActiveRecord::Base
   def self.tests_named(category_name)
-    joins('JOIN categories ON tests.category_id = categories.id').where(categories: { title: category_name }).order(type: :desc)
+    joins('JOIN categories ON tests.category_id = categories.id').where(categories: { title: category_name }).order(type: :desc).pluck(:tittles)
   end
 end
 
