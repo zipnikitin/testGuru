68a31de8404632e910c5838e2f913127bcb2c44b
zipnikitin <zipnikitin@gmail.com>
2021-06-21 18:43:13 +0300

    Second

--- testGuru/app/models/test.rb
+++ testGuru/app/models/test.rb
@@ -1,13 +1,17 @@
 
 class Test < ActiveRecord::Base
-  has_many :questions
-  has_many :results
-  has_many :users, through: :results
   belongs_to :category
-  belongs_to :author, class_name: 'User'
+
+  has_many :questions, dependent: :destroy
+  has_many :results, dependent: :destroy
+  has_many :users, through: :results
+
 
   def self.tests_named(category_name)
-    joins('JOIN categories ON tests.category_id = categories.id').where(categories: { title: category_name }).order(type: :desc).pluck(:tittles)
+    joins(:category)
+      .where(categories: { title: category_title })
+      .order(title: :desc)
+      .pluck(:title)
   end
 end
 
--- testGuru/app/models/user.rb
+++ testGuru/app/models/user.rb
@@ -1,11 +1,12 @@
 
 class User < ActiveRecord::Base
-  has_many :results
-  has_many :tests, through: :results
-  has_many :tests, foreign_key: :author_id, class_name: 'Test'
+  has_many :results, dependent: :destroy
+  has_many :tests, through: :results, dependent: :destroy
 
   def tests_with_level(current_level)
-    Test.joins('JOIN results ON tests.id = results.test_id').where( results: { user_id: id }, level: current_level )
+    Test.joins(:result)
+      .where(results: { user_id: id })
+      .where(level: current_level)
   end
 end
 
