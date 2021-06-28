1a5dfca46a962de8d419f02fe6aef9e53e792a14
zipnikitin <zipnikitin@gmail.com>
2021-06-28 13:21:17 +0300

    Secondary second

--- testGuru/app/models/test.rb
+++ testGuru/app/models/test.rb
@@ -1,14 +1,17 @@
 
-class Test < ActiveRecord::Base
+class Test < ActiveRecord::Base 
+  belongs_to :category
+  belongs_to :author, class_name: "User", foreign_key: "author_id"
+
   has_many :questions, dependent: :destroy
   has_many :results, dependent: :destroy
   has_many :users, through: :results, dependent: :destroy
 
-  belongs_to :category
-  belongs_to :author, class_name: "User", foreign_key: "author_id"
-
   def self.tests_named(category_name)
-    joins('JOIN categories ON tests.category_id = categories.id').where(categories: { title: category_name }).order(type: :desc).pluck(:tittles)
+    joins(:category)
+      .where(categories: { title: category_title })
+      .order(title: :desc)
+      .pluck(:title)
   end
 end
 
