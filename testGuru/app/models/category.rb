d1cae2468039aa318d13f0c78fd78c7cc9a2b825
zipnikitin <zipnikitin@gmail.com>
2021-06-29 18:48:59 +0300

    First

--- testGuru/app/models/category.rb
+++ testGuru/app/models/category.rb
@@ -1,3 +1,8 @@
 class Category < ApplicationRecord
   has_many :tests, dependent: :destroy
+
+  validates :title, presence: true
+
+  default_scope { order(title: :asc) }
+  
 end
--- testGuru/app/models/test.rb
+++ testGuru/app/models/test.rb
@@ -7,11 +7,20 @@ class Test < ActiveRecord::Base
   has_many :results, dependent: :destroy
   has_many :users, through: :results
 
-  def self.tests_named(category_name)
+  validates :tittle, presence: true, uniqueness: { scope: :level }
+
+  validates :level, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
+  scope :easy_tests,    -> { level(0..1) }
+  scope :medium_tests,  -> { level(2..4) }
+  scope :hard_tests,    -> { level(5..Float::INFINITY) }
+
+  scope :tests_named, ->(category_name) do
     joins(:category)
       .where(categories: { title: category_name })
       .order(title: :desc)
       .pluck(:title)
   end
+
 end
 
